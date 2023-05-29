165552b435f8af336e0e761aee40ca24ae7e8d84
Dux <kartikmadaan1907@gmail.com>
2023-05-29 15:45:15 +0530

    DRY

--- airbnb/src/App.jsx
+++ airbnb/src/App.jsx
@@ -4,31 +4,36 @@ import "./App.css";
 
 function App() {
   const [active, setActive] = useState("HTML");
+  const object1 = [
+    {
+      label: "HTML",
+      content:
+        "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.",
+    },
+    {
+      label: "CSS",
+      content:
+        "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML.",
+    },
+    {
+      label: "JavaScript",
+      content:
+        "JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.",
+    },
+  ];
   return (
     <>
-      <button onClick={() => setActive("HTML")} className = {active == 'HTML' && 'active'}>HTML</button>
-      <button onClick={() => setActive("CSS")} className = {active == 'CSS' && 'active'}>CSS</button>
-      <button onClick={() => setActive("JavaScript")} className = {active == 'JavaScript' && 'active'}>JavaScript</button>
-      {active == "HTML" && (
-        <p>
-          The HyperText Markup Language or HTML is the standard markup language
-          for documents designed to be displayed in a web browser.
-        </p>
-      )}
-
-      {active == "CSS" && (
-        <p>
-          Cascading Style Sheets is a style sheet language used for describing
-          the presentation of a document written in a markup language such as
-          HTML or XML.
-        </p>
-      )}
-      {active == "JavaScript" && (
-        <p>
-          JavaScript, often abbreviated as JS, is a programming language that is
-          one of the core technologies of the World Wide Web, alongside HTML and
-          CSS.
-        </p>
+      {object1.map((val) => (
+        <button
+          key={val.label}
+          onClick={() => setActive(val.label)}
+          className={val.label == active && "active1"}
+        >
+          {val.label}
+        </button>
+      ))}
+      {object1.map(
+        (val) => active == val.label && <p key={val.label}>{val.content}</p>
       )}
     </>
   );
